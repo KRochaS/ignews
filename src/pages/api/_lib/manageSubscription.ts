1d6c7c1aac9abdf9181b0454857e2727f4b462f2
KRochaS <karinerocha.s@hotmail.com>
2023-06-29 20:08:09 -0300

    manageSubscription

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -1,19 +1,66 @@
 // Todo arquivo dentro da pasta api se torna uma api route.
 // Adicionando _ na frente, a pasta não é tratada como uma rota da 
 // aplicação
-
+import { query as q } from 'faunadb';
 import { fauna } from "../../../services/fauna";
+import { stripe } from '../../../services/stripe';
+
 
 
 export async function saveSubscription(
-    SubscriptionId: string,
+    subscriptionId: string,
     customerId: string,
+    createdAction = false
 ) {
     // Buscar o usuário no banco do FaunaDB com o ID {customerId}
-
-    // const userRef = await fauna.query(
-        
-    // )
+    const userRef = await fauna.query(
+      q.Select(
+        "ref",
+        q.Get(
+            q.Match(
+                q.Index('user_by_stripe_customer_id'),
+                customerId
+            )
+        )
+      )
+    )
 
     // Salvar os dados da subscription no FaunaDB
+
+    const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+    const subscriptionData = {
+        id: subscription.id,
+        userId: userRef,
+        status: subscription.status,
+        price_id: subscription.items.data[0].price.id
+    }
+
+
+    if(createdAction) {
+        await fauna.query(
+            q.Create(
+                q.Collection('subscriptions'),
+                {data: subscriptionData}
+            )
+        )
+    } else {
+        await fauna.query(
+            q.Replace(
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index('subscription_by_id'),
+                            subscriptionId
+                        )
+                    )
+                ),
+                {
+                    data: subscriptionData
+                }
+            )
+        )
+    }
+   
+    
 }
