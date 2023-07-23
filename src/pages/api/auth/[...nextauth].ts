f1c6f90fed7812041cc6989f66545a8362a85cb0
KRochaS <karinerocha.s@hotmail.com>
2023-07-23 19:35:57 -0300

    Next Auth - Session Callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -16,6 +16,42 @@ export const authOptions = {
         secret: process.env.SIGNING_KEY
     },
     callbacks: {
+        async session(session) {
+            try {
+                const userActiveSubscription = await fauna.query(
+                    q.Get(
+                        q.Intersection([
+                            q.Match(
+                                q.Index('subscription_by_user_ref'),
+                                q.Select(
+                                    "ref",
+                                    q.Get(
+                                        q.Match(
+                                            q.Index('user_by_email'),
+                                            q.Casefold(session.user.email)
+                                        )
+                                    )
+                                )
+                            ),
+                            q.Match(
+                                q.Index('subscription_by_status'),
+                                "active"
+                            )
+                        ])
+                    )
+                )
+
+                return {
+                    ...session.session,
+                    activeSubscription: userActiveSubscription
+                };
+            } catch {
+                return {
+                    ...session.session,
+                    activeSubscription: null
+                };
+            }
+        },
         async signIn({ user }) {
             const { email } = user;
 
