9a49da7e34c36fa1ebb108f8c57c163ea001657f
KRochaS <karinerocha.s@hotmail.com>
2023-11-26 19:07:19 -0300

    feat: Formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,41 @@
-import Head from 'next/head';
-import { GetServerSideProps, GetStaticProps } from 'next';
-import { SubscribeButton } from '../components/SubscribeButton';
-import styles from './home.module.scss';
-import { stripe } from '../services/stripe';
+import { GetStaticProps } from "next";
+import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
 
 interface HomeProps {
-    product: {
-        priceId: string;
-        amount: number;
-    }
+  product: {
+    priceId: string;
+    amount: string;
+  };
 }
 
 export default function Home({ product }: HomeProps) {
-    return (
-        <>
-            <Head>
-                <title> Home | ig.news </title>
-            </Head>
-            <main className={styles.contentContainer}>
-                <section className={styles.hero}>
-                    <span> 👏 Hey, welcome </span>
-                    <h1> News about  <br /> the <span>React</span> world.</h1>
-                    <p>
-                        Get access to all the publications <br />
-                        <span> for {product.amount} month</span>
-                    </p>
+  return (
+    <>
+      <Head>
+        <title> Home | ig.news </title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, welcome </span>
+          <h1>
+            {" "}
+            News about <br /> the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span> for {product.amount} month</span>
+          </p>
 
-                    <SubscribeButton priceId={product.priceId}/>
-                </section>
+          <SubscribeButton />
+        </section>
 
-                <img src="/images/avatar.svg" alt="Girl coding" />
-            </main>
-        </>
-    )
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
 
 // SSR - Server Side Rendering
@@ -56,30 +59,28 @@ export default function Home({ product }: HomeProps) {
 //     }
 // }
 
-
 // SSG - Static Site Generation
 // Uma nova camada que após passar pelo servidor node, salva o HTML de
 // forma estática para não fazer todo o processo novamente.
 // Processo é revalidado a cada tempo que é setado no revalidate
 
 export const getStaticProps: GetStaticProps = async () => {
-    const price = await stripe.prices.retrieve('price_1Mlh3mHUOpTgsE9bHctyXqla', {
-        expand: ['product'] // expand -> obter todas as informações do produto
-    })
+  const price = await stripe.prices.retrieve("price_1Mlh3mHUOpTgsE9bHctyXqla", {
+    expand: ["product"], // expand -> obter todas as informações do produto
+  });
 
-    const product = {
-        priceId: price.id,
-        amount: new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: 'USD',
-        }).format(price.unit_amount / 100,)
-    }
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
 
-    return {
-        props: {
-            product,
-        },
-        revalidate: 60 * 60 * 24 // 24h
-    }
-}
-
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24h
+  };
+};
